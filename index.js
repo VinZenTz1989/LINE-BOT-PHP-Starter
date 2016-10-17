d929f80a8acf1c9fb5b9abbbfdfb19c5fe07d75b
Boss <s.sittilerdpaisal@gmail.com>
2016-10-17 12:10:29 +0700

    e

--- index.js
+++ index.js
@@ -8,35 +8,12 @@ const bot = linebot({
     channelAccessToken: "hr2r8ePdDroHXqjqoVvbT8MSP0umxs1obr9vik9tTs9KYGIaK8xvbEZ53m52JdX73MSqjryJBiblK9CGNUDOP2kozlg/Cksx3dGoX94oMmrxvoLM+CTwlhSKO6bdcrZMSopXLFkqroQKcjs/DdWHGAdB04t89/1O/w1cDnyilFU=", verify: false // default=true
 });
 
-const app = express();
-
-const parser = bodyParser.json({
-	verify: function (req, res, buf, encoding) {
-		req.rawBody = buf.toString(encoding);
-	}
-});
-
-app.post('/linewebhook', parser, function (req, res) {
-	if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
-		return res.sendStatus(400);
-	}
-	bot.parse(req.body);
-	return res.json({});
-});
-
-app.get('/', parser, function (req, res) {
-	return res.json({boss:'Boss'});
-});
-
 bot.on('message', function (event) {
-
-	event.reply(event.message.text).then(function (data) {
-		console.log('Success', data);
-	}).catch(function (error) {
-		console.log('Error', error);
-	});
+    event.reply(event.message.text).then(function (data) {
+        // success
+    }).catch(function (error) {
+        // error
+    });
 });
 
-app.listen(process.env.PORT || 80, function () {
-	console.log('LineBot is running on port 4000');
-});
+bot.listen('/linewebhook', 3000);
