125251811c02d11c5ee08c26a8ca108ffc1ac21e
Boss <s.sittilerdpaisal@gmail.com>
2016-10-17 13:22:49 +0700

    i

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const bot = linebot({
     channelId: "1484524579",
     channelSecret: "d37936b83102309d5a7dcc6dc46db5a7",
-    channelAccessToken: "hr2r8ePdDroHXqjqoVvbT8MSP0umxs1obr9vik9tTs9KYGIaK8xvbEZ53m52JdX73MSqjryJBiblK9CGNUDOP2kozlg/Cksx3dGoX94oMmrxvoLM+CTwlhSKO6bdcrZMSopXLFkqroQKcjs/DdWHGAdB04t89/1O/w1cDnyilFU=", verify: false // default=true
+    channelAccessToken: "hr2r8ePdDroHXqjqoVvbT8MSP0umxs1obr9vik9tTs9KYGIaK8xvbEZ53m52JdX73MSqjryJBiblK9CGNUDOP2kozlg/Cksx3dGoX94oMmrxvoLM+CTwlhSKO6bdcrZMSopXLFkqroQKcjs/DdWHGAdB04t89/1O/w1cDnyilFU=", 
+	verify: true // default=true
 });
 
 bot.on('message', function (event) {
